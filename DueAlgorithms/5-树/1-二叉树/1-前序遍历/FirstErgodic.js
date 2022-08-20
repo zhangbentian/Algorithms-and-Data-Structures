53ba0eb29cec02780480e168a033429c53efdc96
zbt <2479879758@qq.com>
2022-08-20 17:33:20 +0800

    前序遍历

--- DueAlgorithms/5-树/1-二叉树/1-前序遍历/FirstErgodic.js
+++ DueAlgorithms/5-树/1-二叉树/1-前序遍历/FirstErgodic.js
@@ -2,12 +2,13 @@
  * @Description: 有问题,联系qq:2479879758
  * @Author: zhulinhai
  * @LastEditTime: 2022-08-01 11:03:53
+ * 
  */
 class DichotomousTree {
     left = null;
     right = null;
-    constructor(value) {
-        this.value = value;
+    constructor(val = 0) {
+        this.val = val;
 
     }
 }
@@ -28,10 +29,52 @@ b.right = e;
 
 // 前序遍历
 function ergodic(root) {
-    if(root === null) return;
-    console.log(root.value);
+    if (root === null) return;
+    console.log(root.val);
     ergodic(root.left);
     ergodic(root.right);
 }
 
-ergodic(a);
+// ergodic(a);
+
+// 力扣 https://leetcode.cn/problems/binary-tree-preorder-traversal/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+
+class nodeTree{
+    left = null;
+    right = null;
+    constructor(val = 0) {
+        this.val = val;
+    }
+}
+
+let node1 = new nodeTree(1);
+let node2 = new nodeTree(2);
+let node3 = new nodeTree(3);
+node1.right = node2;
+node2.left = node3;
+
+var preorderTraversal = function (root) {
+    let res = [];
+    const FirstErgodic = (root)=>{
+        if(!root) return;
+        res.push(root.val)
+        FirstErgodic(root.left)
+        FirstErgodic(root.right)
+    }
+    FirstErgodic(root)
+    return res;
+};
+console.log(preorderTraversal(node1));

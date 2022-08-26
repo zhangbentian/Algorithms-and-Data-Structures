fc6934390fd535187471c403f6cda3d2b64ccf1e
zbt <2479879758@qq.com>
2022-08-26 09:34:19 +0800

    2-二叉树的层序遍历

---
+++ DueAlgorithms/10-树的算法/2.中等/2-二叉树的层序遍历/index.js
@@ -0,0 +1,22 @@
+//bfs
+//https://leetcode.cn/problems/binary-tree-level-order-traversal/
+var levelOrder = function(root) {
+    const ret = [];
+    if (!root) {
+        return ret;
+    }
+
+    const q = [];
+    q.push(root);
+    while (q.length !== 0) {
+        const currentLevelSize = q.length;
+        ret.push([]); // 牛批
+        for (let i = 1; i <= currentLevelSize; ++i) {
+            const node = q.shift();
+            ret[ret.length - 1].push(node.val);
+            if (node.left) q.push(node.left);
+            if (node.right) q.push(node.right);
+        }
+    }
+    return ret;
+};
